df33612e1a3d371b5aa808bb17f6d55d2c40ce7e
ylak963 <pepitogrilloreturn@gmail.com>
2019-10-21 08:45:18 +0200

    Version 0.0 Random number

---
+++ Aleatorios/src/es/studium/Aleatorios/Aleatorios.java
@@ -0,0 +1,13 @@
+package es.studium.Aleatorios;
+
+public class Aleatorios {
+
+		public static void main(String[] args)
+		{
+			//Números aleatorios
+			System.out.println("Numero aleatorio:"+ Math.random());
+			System.out.println("Otro número aleatorio:" + Math.random());
+		}
+
+	}
+
